fe7ef05f5b40adabd9c7a04e94ceba456886a690
Lorranyy <capic.2022118isinf0799@aluno.ifpi.edu.br>
2023-09-24 14:31:46 -0300

    js adicionado e atualizações nos planos

---
+++ script.js
@@ -0,0 +1,22 @@
+//js dos planos
+const botao = document.querySelector('.conheca');
+const divConteudo = document.querySelector('.conteudo');
+
+botao.addEventListener('click', () => {
+    divConteudo.classList.toggle('oculta');
+})
+
+const botao1 = document.querySelector('.conheca1');
+const divConteudo1 = document.querySelector('.conteudo1');
+
+botao1.addEventListener('click', () => {
+    divConteudo1.classList.toggle('oculta1');
+})
+
+const botao2 = document.querySelector('.conheca2');
+const divConteudo2 = document.querySelector('.conteudo2');
+
+botao2.addEventListener('click', () => {
+    divConteudo2.classList.toggle('oculta2');
+})
+
