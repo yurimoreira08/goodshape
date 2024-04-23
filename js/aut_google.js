bcd3eacab4d3e8ec323097a175d3d18b7e28f8bd
Yuri <capic.2022118isinf0381@aluno.ifpi.edu.br>
2024-04-23 19:45:23 -0300

    :rocket:Começando autenticação com o google

--- js/aut_google.js
+++ js/aut_google.js
@@ -25,7 +25,7 @@ async function handleSignInWithGoogle(response) {
   const nonce = crypto.randomBytes(16).toString('base64') // Generate a random nonce
   const encoder = new TextEncoder()
   const encodedNonce = encoder.encode(nonce) // Encode the nonce
-  const hash = await crypto.subtle.digest('SHA-256', encodedNonce) // Hash it with SHA-256
+  const hash = crypto.subtle.digest('SHA-256', encodedNonce) // Hash it with SHA-256
   const bytes = new Uint8Array(hash)
   const hashedNonce = Array.from(bytes)
     .map((b) => b.toString(16).padStart(2, '0')) // Convert the hash to a hexadecimal string
