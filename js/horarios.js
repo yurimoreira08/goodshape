75da28e2ef78ac8424a223285ee33b61139d53be
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-08-12 01:20:44 -0300

    Update horarios.js

--- js/horarios.js
+++ js/horarios.js
@@ -4,47 +4,45 @@ const horaInicioInput = document.getElementById('hora-inicio-input');
 const horaFimInput = document.getElementById('hora-fim-input');
 const confirmarAgendamento = document.getElementById('confirmar-agendamento');
 
-confirmarAgendamento.addEventListener('click', async () => {
-  const selectedDate = document.querySelector('.calendar .day.selected')?.dataset.date;
-  const selectedHoraInicio = horaInicioInput.value;
-  const selectedHoraFim = horaFimInput.value;
+confirmarAgendamento.onclick = async () => {
+  const diaSemana = document.querySelector('.calendar .day.selected')?.dataset.date;
+  const horaInicio = horaInicioInput.value;
+  const horaFim = horaFimInput.value;
+ 
+ 
+  console.log("dados", diaSemana, horaInicio, horaFim);
 
-  if (!selectedDate) {
-      alert('Por favor, selecione uma data.');
-      return;
+
+  if (!horaInicio || !horaFim) {
+    alert('Por favor, insira os horários de início e fim.');
+    return;
   }
 
-  if (!selectedHoraInicio || !selectedHoraFim) {
-      alert('Por favor, insira os horários de início e fim.');
-      return;
+  if (horaInicio >= horaFim) {
+    alert('O horário de início deve ser antes do horário de fim.');
+    return;
   }
 
-  if (selectedHoraInicio >= selectedHoraFim) {
-      alert('O horário de início deve ser antes do horário de fim.');
-      return;
-  }
+  const agendamento = { diaSemana, horaInicio, horaFim};
 
   try {
-      const resposta = await fetch('https://back-end-live-in-shape-1.onrender.com/salvarHorarios', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${localStorage.getItem('token')}` // Adicione o token de autenticação se necessário
-          },
-          body: JSON.stringify({
-              date: selectedDate,
-              inicio: selectedHoraInicio,
-              fim: selectedHoraFim,
-              profissional: 'Danielle Silva' // Substitua com o nome do profissional real
-          })
-      });
+    const chamada = await fetch(URL2, {
+      method: 'POST',
+      body: JSON.stringify(agendamento),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+       
+      }),
+    });
+    
+    if (!chamada.ok) {
+      throw new Error('Erro ao salvar horário.');
+    }
 
-      if (!resposta.ok) {
-          throw new Error('Erro ao salvar a disponibilidade.');
-      }
-
-      alert('Horário definido com sucesso!');
+    console.log('Chamada:', chamada);
+    alert('Horário salvo com sucesso!');
   } catch (erro) {
-      console.error('Erro ao salvar disponibilidade:', erro.message);
+    console.error(erro);
+    alert('Erro ao salvar horário.');
   }
-});
+}
