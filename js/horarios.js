72fcb238b52fa840382f35d3722ef4819629dd70
Yuri Moreira <128077291+yurimoreira08@users.noreply.github.com>
2024-08-11 22:48:57 -0300

    Update horarios.js

--- js/horarios.js
+++ js/horarios.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://back-end-live-in-shape-1.onrender.com/salvarHorarios/';
+const URL = 'https://back-end-live-in-shape-1.onrender.com/horarios/';
 const dia = document.querySelector('.calendar .day.selected')?.dataset.date;
 const horaInicioInput = document.getElementById('hora-inicio-input');
 const horaFimInput = document.getElementById('hora-fim-input');
@@ -8,19 +8,26 @@ confirmarAgendamento.onclick = async () => {
     const diaSemana = dia;
     const horaInicio = horaInicioInput.value;
     const horaFim = horaFimInput.value;
-    const usuarioId = localStorage.getItem('usuarioId');
- 
-  
-    const agendamento = { diaSemana, horaInicio, horaFim, usuarioId };
-  
-    const chamada = await fetch(apiURL, {
-      method: 'POST',
-      body: JSON.stringify(agendamento),
-      headers: new Headers({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`, // Envie o token aqui
-      }),
-    });
-    console.log('chamada:', chamada);
-}
+    const usuarioId = localStorage.getItem('usuarioId'); // Certifique-se de que o 'usuarioId' está armazenado corretamente
 
+    const agendamento = { diaSemana, horaInicio, horaFim, usuarioId };
+
+    try {
+        const chamada = await fetch(URL, {
+            method: 'POST',
+            body: JSON.stringify(agendamento),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`, // Inclua o token de autenticação
+            }),
+        });
+
+        if (!chamada.ok) {
+            throw new Error(`Erro ao enviar solicitação: ${chamada.statusText}`);
+        }
+
+        console.log('Resposta da chamada:', await chamada.text());
+    } catch (erro) {
+        console.error('Erro ao chamar a API:', erro);
+    }
+};
