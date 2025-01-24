a8a3750011ec91b9bb48b5b305910b2418ba87ce
Yuri <capic.2022118isinf0381@aluno.ifpi.edu.br>
2025-01-24 20:03:39 -0300

    teste

--- js/dicas.js
+++ js/dicas.js
@@ -1,4 +1,4 @@
-const response = await fetch('https://back-end-live-in-shape.onrender.com/src/rotas/dica', {
+const response = await fetch('https://back-end-live-in-shape-1.onrender.com/src/rotas/dica', {
     method: 'POST', 
     headers: {
         'Content-Type': 'application/json',
