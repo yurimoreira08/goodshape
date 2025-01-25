0b24aeab696fd33d71e755cfa7a2c251ad1119d4
Lorranyy <capic.2022118isinf0799@aluno.ifpi.edu.br>
2025-01-25 09:04:38 -0300

    teste

--- js/dicas.js
+++ js/dicas.js
@@ -1,4 +1,4 @@
-const response = await fetch('https://back-end-live-in-shape-1.onrender.com/profissional/adicionar_dicas', {
+const response = await fetch('https://back-end-live-in-shape-1.onrender.com/dica/adicionar_dicas', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
